26be9162a9c23b190c3757362fb8c645bcbf0b68
Jmin0613 <90593474+Jmin0613@users.noreply.github.com>
2021-12-19 22:26:04 +0900

    Add files via upload

---
+++ 비주얼프로그래밍/4주차/4주차과제_사칙계산기.sln/4주차과제_사칙계산기/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿
+namespace _4주차과제_사칙계산기
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNo1 = new System.Windows.Forms.TextBox();
+            this.txtNo2 = new System.Windows.Forms.TextBox();
+            this.btnPlus = new System.Windows.Forms.Button();
+            this.btnMinus = new System.Windows.Forms.Button();
+            this.btnTimes = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "숫자1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "숫자2";
+            // 
+            // txtNo1
+            // 
+            this.txtNo1.Location = new System.Drawing.Point(113, 54);
+            this.txtNo1.Name = "txtNo1";
+            this.txtNo1.Size = new System.Drawing.Size(108, 21);
+            this.txtNo1.TabIndex = 2;
+            // 
+            // txtNo2
+            // 
+            this.txtNo2.Location = new System.Drawing.Point(113, 94);
+            this.txtNo2.Name = "txtNo2";
+            this.txtNo2.Size = new System.Drawing.Size(108, 21);
+            this.txtNo2.TabIndex = 3;
+            // 
+            // btnPlus
+            // 
+            this.btnPlus.Location = new System.Drawing.Point(50, 156);
+            this.btnPlus.Name = "btnPlus";
+            this.btnPlus.Size = new System.Drawing.Size(42, 42);
+            this.btnPlus.TabIndex = 4;
+            this.btnPlus.Text = "+";
+            this.btnPlus.UseVisualStyleBackColor = true;
+            this.btnPlus.Click += new System.EventHandler(this.btnPlus_Click);
+            // 
+            // btnMinus
+            // 
+            this.btnMinus.Location = new System.Drawing.Point(98, 156);
+            this.btnMinus.Name = "btnMinus";
+            this.btnMinus.Size = new System.Drawing.Size(42, 42);
+            this.btnMinus.TabIndex = 5;
+            this.btnMinus.Text = "-";
+            this.btnMinus.UseVisualStyleBackColor = true;
+            this.btnMinus.Click += new System.EventHandler(this.btnMinus_Click);
+            // 
+            // btnTimes
+            // 
+            this.btnTimes.Location = new System.Drawing.Point(146, 156);
+            this.btnTimes.Name = "btnTimes";
+            this.btnTimes.Size = new System.Drawing.Size(42, 42);
+            this.btnTimes.TabIndex = 6;
+            this.btnTimes.Text = "*";
+            this.btnTimes.UseVisualStyleBackColor = true;
+            this.btnTimes.Click += new System.EventHandler(this.btnTimes_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Location = new System.Drawing.Point(194, 156);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(42, 42);
+            this.btnDivide.TabIndex = 7;
+            this.btnDivide.Text = "/";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(63, 256);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(29, 12);
+            this.lblResult.TabIndex = 8;
+            this.lblResult.Text = "결과";
+            this.lblResult.Click += new System.EventHandler(this.lblResult_Click);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(113, 253);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(97, 21);
+            this.txtResult.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(282, 362);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnTimes);
+            this.Controls.Add(this.btnMinus);
+            this.Controls.Add(this.btnPlus);
+            this.Controls.Add(this.txtNo2);
+            this.Controls.Add(this.txtNo1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "사칙계산기";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNo1;
+        private System.Windows.Forms.TextBox txtNo2;
+        private System.Windows.Forms.Button btnPlus;
+        private System.Windows.Forms.Button btnMinus;
+        private System.Windows.Forms.Button btnTimes;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
---
+++ 비주얼프로그래밍/4주차/4주차과제_사칙계산기.sln/4주차과제_사칙계산기/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace _4주차과제_사칙계산기
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPlus_Click(object sender, EventArgs e)
+        {
+            double Plus = double.Parse(txtNo1.Text) + double.Parse(txtNo2.Text);
+
+            txtResult.Text = Plus.ToString();
+            lblResult.Text = "(+)결과";
+
+        }
+
+        private void btnMinus_Click(object sender, EventArgs e)
+        {
+            double Minus = double.Parse(txtNo1.Text) - double.Parse(txtNo2.Text);
+            txtResult.Text = Minus.ToString();
+            lblResult.Text = "(-)결과";
+        }
+
+        private void btnTimes_Click(object sender, EventArgs e)
+        {
+            double Times = double.Parse(txtNo1.Text) * double.Parse(txtNo2.Text);
+            txtResult.Text = Times.ToString();
+            lblResult.Text = "(*)결과";
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            double Divide = double.Parse(txtNo1.Text) / double.Parse(txtNo2.Text);
+            txtResult.Text = Divide.ToString("0.00");
+            lblResult.Text = "(/)결과";
+        }
+
+        private void lblResult_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
