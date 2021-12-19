6343e5172f46557458ba109578d8d79eede71525
Jmin0613 <90593474+Jmin0613@users.noreply.github.com>
2021-12-20 00:18:23 +0900

    Add files via upload

---
+++ 비주얼프로그래밍/기말 비주얼(강의)/W005_UserControl/W005_UserControl/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace W005_UserControl
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRed_Click(object sender, RoutedEventArgs e)
+        {
+            btnColor.Background = Brushes.Red;
+        }
+
+        private void btnGreen_Click(object sender, RoutedEventArgs e)
+        {
+            btnColor.Background = Brushes.Green;
+        }
+
+        private void btnBlue_Click(object sender, RoutedEventArgs e)
+        {
+            btnColor.Background = Brushes.Blue;
+        }
+    }
+}
---
+++ 비주얼프로그래밍/기말 비주얼(강의)/W007_UniformGrid/W007_UniformGrid/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace W007_UniformGrid
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            ChessBoard.Rows = 8;
+            ChessBoard.Columns = 8;
+
+            for (int i = 0; i < 64 / 2; i++)
+            {
+                // 짝수줄은 검은색, 빨간색
+                // 홀수줄은 빨간색, 검은색
+                if ((i / 4) % 2 == 0) // 짝수줄
+                {
+                    Rectangle r1 = new Rectangle();
+                    r1.Fill = Brushes.Black;
+                    ChessBoard.Children.Add(r1);
+
+                    Rectangle r2 = new Rectangle();
+                    r2.Fill = Brushes.Red;
+                    ChessBoard.Children.Add(r2);
+                }
+                else // 홀수줄
+                {
+                    Rectangle r2 = new Rectangle();
+                    r2.Fill = Brushes.Red;
+                    ChessBoard.Children.Add(r2);
+
+                    Rectangle r1 = new Rectangle();
+                    r1.Fill = Brushes.Black;
+                    ChessBoard.Children.Add(r1);
+                }
+            }
+        }
+    }
+}
