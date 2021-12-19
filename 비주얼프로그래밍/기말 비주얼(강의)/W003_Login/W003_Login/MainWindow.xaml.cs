842e1ab207feab49813d35644b4a399e56b223f2
Jmin0613 <90593474+Jmin0613@users.noreply.github.com>
2021-12-20 00:16:42 +0900

    Add files via upload

---
+++ 비주얼프로그래밍/기말 비주얼(강의)/W003_Login/W003_Login/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace W003_Login
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Clicked(object sender, RoutedEventArgs e)
+        {
+            if (txtId.Text == "abcd" && txtPassword.Password == "1234")
+                MessageBox.Show("Login Successful!");
+            else
+                MessageBox.Show("Login Failed!");
+        }
+    }
+}
---
+++ 비주얼프로그래밍/기말 비주얼(강의)/W004_Language/W004_Language/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace W004_Language
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CheckBox[] cbs;
+        public MainWindow()
+        {
+            InitializeComponent();
+            cbs = new CheckBox[] { cbC, cbCPP, cbCS, cbPy, cbJava };
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            String str = string.Empty;
+
+            foreach(var cb in cbs)
+            {
+                if (cb.IsChecked == true)
+                    str += cb.Content + " ";
+            }
+
+            MessageBox.Show(str + "Selected!", "Language Prefered!");
+        }
+
+        private void btnQuit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
