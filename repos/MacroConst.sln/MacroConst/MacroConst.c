cdafd6ea6c139d29f96253ff82809e328af2704f
Jmin0613 <90593474+Jmin0613@users.noreply.github.com>
2021-12-19 23:19:07 +0900

    Add files via upload

---
+++ repos/MacroConst.sln/MacroConst/MacroConst.c
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+#define NAME "홍길동"
+#define AGE	24
+#define PRINT_ADDR	puts("주소: 대전광역시\n");
+
+int main() {
+	printf("이름: %s \n", NAME);
+	printf("나이: %s \n", AGE);
+	PRINT_ADDR;
+	return 0;
+}
