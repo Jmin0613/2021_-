9d871bce64bc2e48bcb1cff1ca8a08df3a0b3d4d
Jmin0613 <90593474+Jmin0613@users.noreply.github.com>
2021-12-20 00:20:06 +0900

    Add files via upload

---
+++ 비주얼프로그래밍/(비주얼 기말) 교재/A129_FormClass/A129_FormClass/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace A129_FormClass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.ClientSize = new Size(500, 500);
+
+            Form f2 = new Form2(); //Form2 클래스의 객체 f2를 생성
+            this.AddOwnedForm(f2); //AddOwnedForm : 다른 폼을 현재 폼에 추가시킴
+
+            f2.Show();
+        }
+    }
+}
---
+++ 비주얼프로그래밍/(비주얼 기말) 교재/A129_FormClass/A129_FormClass/Form2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace A129_FormClass
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+            this.ClientSize = new Size(300, 200);
+        }
+
+        private void Form2_Load(object sender, EventArgs e) //Form2가 화면에 Load될 때 실행되는 이벤트 메소드
+        {
+            CenterToParent(); //CenterToParent : 부모 폼의 중앙에 현재 폼 위치시킴
+        }
+    }
+}
