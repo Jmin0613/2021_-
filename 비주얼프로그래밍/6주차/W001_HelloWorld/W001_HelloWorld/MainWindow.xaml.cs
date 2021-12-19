27b3e38ffd68155bde61f769f6052a3f5e022737
Jmin0613 <90593474+Jmin0613@users.noreply.github.com>
2021-12-19 23:51:57 +0900

    Add files via upload

---
+++ 비주얼프로그래밍/6주차/W001_HelloWorld/W001_HelloWorld/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace W001_HelloWorld
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void TextBlock_MouseDown_1(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("텍스트블록이 클릭되었음.", "Text cliked");
+            if (grid1.Background == Brushes.Orange)
+                grid1.Background = Brushes.AliceBlue;
+            else
+                grid1.Background = Brushes.Orange;
+        }
+    }
+}
